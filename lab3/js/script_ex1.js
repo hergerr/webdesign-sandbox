600a0e5ec9bff9fec9059a8d5a0d32514dcf765d
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-04-20 23:03:04 +0200

    Finished paint

--- lab3/js/script_ex1.js
+++ lab3/js/script_ex1.js
@@ -1,12 +1,56 @@
+// getting elements
 var canvas = document.getElementById('paint-canvas');
+var rect = document.getElementById("rectangle");
+var filledRect = document.getElementById("filled-rectangle");
+var circle = document.getElementById("circle");
+var filledCircle = document.getElementById("filled-circle");
+var colorPicker = document.getElementById("color-control");
+
+// setting initial values
 var ctx = canvas.getContext('2d');
-//Variables
 var canvasx = canvas.offsetLeft;
 var canvasy = canvas.offsetTop;
 var last_mousex = last_mousey = 0;
 var mousex = mousey = 0;
 var mousedown = false;
+var currentShape = "rectangle";
+rect.classList.add("active");
 
+// opcja bez wymazywania
+// http://jsfiddle.net/m1erickson/7uNfW/
+
+// event listeners
+rect.addEventListener("click", function (e) {
+    currentShape = "rectangle";
+    rect.classList.add("active");
+    filledRect.classList.remove("active");
+    circle.classList.remove("active");
+    filledCircle.classList.remove("active");
+})
+
+filledRect.addEventListener("click", function(e){
+    currentShape = "filledRect";
+    filledRect.classList.add("active");
+    rect.classList.remove("active");
+    circle.classList.remove("active");
+    filledCircle.classList.remove("active");
+})
+
+circle.addEventListener("click" , function(e){
+    currentShape = "circle";
+    circle.classList.add("active");
+    rect.classList.remove("active");
+    filledRect.classList.remove("active");
+    filledCircle.classList.remove("active");
+})
+
+filledCircle.addEventListener("click", function(e){
+    currentShape = "filledCircle";
+    filledCircle.classList.add("active");
+    rect.classList.remove("active");
+    filledRect.classList.remove("active");
+    circle.classList.remove("active");
+})
 
 canvas.addEventListener("mousedown", function (e) {
     last_mousex = parseInt(e.pageX - canvasx);
@@ -18,7 +62,6 @@ canvas.addEventListener("mouseup", function (e) {
     mousedown = false;
 })
 
-//Mousemove
 canvas.addEventListener('mousemove', function (e) {
     mousex = parseInt(e.pageX - canvasx);
     mousey = parseInt(e.pageY - canvasy);
@@ -27,9 +70,29 @@ canvas.addEventListener('mousemove', function (e) {
         ctx.beginPath();
         var width = mousex - last_mousex;
         var height = mousey - last_mousey;
-        ctx.rect(last_mousex, last_mousey, width, height);
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 1;
-        ctx.stroke();
+
+        if (currentShape == "rectangle"){
+            ctx.rect(last_mousex, last_mousey, width, height);
+            ctx.strokeStyle = colorPicker.value;
+            ctx.lineWidth = 1;
+            ctx.stroke();
+        } else if (currentShape == "filledRect"){
+            ctx.rect(last_mousex, last_mousey, width, height);
+            ctx.fillStyle = colorPicker.value;
+            ctx.fill();
+        } else if (currentShape == "circle"){
+            ctx.beginPath();
+            // last two args - beginning and ending of curve
+            ctx.strokeStyle = colorPicker.value;
+            ctx.lineWidth = 1;
+            ctx.arc(last_mousex, last_mousey, Math.abs(width), 0, 2 * Math.PI);
+            ctx.stroke();
+        } else if (currentShape == "filledCircle"){
+            ctx.beginPath();
+            ctx.arc(last_mousex, last_mousey, Math.abs(width), 0, 2 * Math.PI);
+            ctx.fillStyle = colorPicker.value;
+            ctx.fill();
+        }
+        
     }
 });
