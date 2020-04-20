4d454cbd014c08576baa2f7eb978f4d26812f7dd
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-04-20 19:56:57 +0200

    Made rectangle drawing

--- lab3/js/script_ex1.js
+++ lab3/js/script_ex1.js
@@ -1,5 +1,30 @@
-class Paint{
-    constructor(){
-        
+var canvas = document.getElementById('paint-canvas');
+var ctx = canvas.getContext('2d');
+
+var canvasX = canvas.offsetLeft;
+var canvasY = canvas.offsetTop;
+
+// console.log(`${canvasY}, ${canvasX} `)
+
+var isDrawing = false;
+var startX;
+var startY;
+
+canvas.addEventListener("mousedown", function (e) {
+    var mouseX = parseInt(e.pageX - canvasX);
+    var mouseY = parseInt(e.pageY - canvasY); 
+
+    if (isDrawing){
+        isDrawing = false;
+        ctx.beginPath();
+        console.log(`${startX}, ${startY}`)
+        ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
+        ctx.fill();
+        canvas.style.cursor = "default";
+    } else {
+        isDrawing = true;
+        startX = mouseX;
+        startY = mouseY;
+        canvas.style.cursor = "crosshair";
     }
-}
+})
