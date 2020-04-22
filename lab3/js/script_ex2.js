3ab595e6d52198e50fb6dc3d1f3a682943e83bc0
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-04-22 12:05:57 +0200

    Added paddle collision

--- lab3/js/script_ex2.js
+++ lab3/js/script_ex2.js
@@ -1,14 +1,23 @@
 var canvas = document.getElementById("paint-canvas");
 var ctx = canvas.getContext("2d");
 
-var ballRadius = 10;
 
 // ball start position
 var x = canvas.width / 2;
 var y = canvas.height - 30;
+var ballRadius = 10;
 // ball speed
 var dx = 2;
 var dy = -2;
+
+
+// paddle dimensions
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall() {
     ctx.beginPath();
@@ -18,17 +27,36 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
     x += dx;
     y += dy;
 
     // collision with bottom and top
     // ballradius because of collision is counted with central point of the ball
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+
+    if (y + dy < ballRadius) {
         dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
     }
 
     // collision with right and left
@@ -36,7 +64,46 @@ function draw() {
         dx = -dx;
     }
 
+    // paddle coordinates changes
+    if (rightPressed) {
+        paddleX += 7;
+
+        // set boundaries from right
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if (leftPressed) {
+        paddleX -= 7;
+
+        // ... and from left
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
+
 
 }
 
-setInterval(draw, 10);
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+var interval = setInterval(draw, 10);
