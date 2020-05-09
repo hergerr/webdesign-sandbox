45652e5e6762f11d4097f59ee327856d5843793e
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-05-09 13:58:22 +0200

    Algorithm works

--- lab4/js/ex2.js
+++ lab4/js/ex2.js
@@ -1,5 +1,31 @@
 exerciseNumber = document.getElementById("exercise-number");
 
-window.onload = function(){
+function solution(A) {
+    var maxHeight = 0
+    var minHeight = 0
+    var maxDepth = 0
+
+    A.forEach(element => {
+        if (element > maxHeight) {
+            var d = maxHeight - minHeight
+            maxHeight = element
+            minHeight = element
+        } else if (element < minHeight) {
+            minHeight = element
+        } else {
+            var d = element - minHeight
+        }
+
+        if (d > maxDepth) {
+            maxDepth = d
+        }
+    });
+
+    return maxDepth
+}
+
+
+window.onload = function () {
     exerciseNumber.innerHTML = `Numer zadania: 241255 mod 5 = ${241255 % 5}`
+    console.log(solution([1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2]))
 }
