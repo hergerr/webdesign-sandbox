2ad4d57f18b8655103b2d380504d30027785d3c0
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-06-04 15:14:21 +0200

    Reload works like charm

--- lab6.2/webpack.config.js
+++ lab6.2/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
