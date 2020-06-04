58a5a7dcbf942332f644536e68631b40554cee34
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-06-04 13:04:30 +0200

    scss and webpack combo works

--- lab6.2/src/index.js
+++ lab6.2/src/index.js
@@ -1,3 +1,4 @@
 const lol = require("./js/main");
+import './style/index.scss'
 
 console.log(lol.lol);
--- lab6.2/webpack.config.js
+++ lab6.2/webpack.config.js
@@ -1,9 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/js'),
-  },
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist/js'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: path.resolve(__dirname, 'dist/index.html')
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/subpage.html'),
+            filename: path.resolve(__dirname, 'dist/subpage.html')
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          },
+        ],
+      },
 };
